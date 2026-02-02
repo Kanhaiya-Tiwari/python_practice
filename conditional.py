b803e72e280781014f036c42bbfec6c5403b2f72
Kanhaiya Tiwari <kanhaiyatiwari506@gmail.com>
2026-02-02 11:44:33 +0530

    push conditional.py

---
+++ conditional.py
@@ -0,0 +1,15 @@
+marks = input("your marks : ")
+if (int(marks) <= 33):
+    print("you are fail")
+elif (int(marks) <=50):
+    print("third division pass")
+elif (int(marks) <=70):
+    print ("second division pass")
+else:
+     print("first division pass")
+
+
+
+
+
+    
