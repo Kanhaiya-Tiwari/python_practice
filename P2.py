dae9c46394bf4f7fddc8311f68924eee77b6f94b
Kanhaiya Tiwari <kanhaiyatiwari506@gmail.com>
2026-02-02 09:52:56 +0530

    push P2.py

--- P2.py
+++ P2.py
@@ -2,3 +2,16 @@ a = 56
 b = 34.5
 c = a+b
 print("The sum of a and b is",c)
+
+d = b-a
+print("The difference of b and a is",d)
+e= a*b
+print("The product of a and b is",e)
+f = b/a
+print("The division of b by a is",f)
+g = a//b
+print("The floor division of a by b is",g)
+h = a**2
+print("The value of a to the power 2 is",h)
+i = a%b
+print("The modulus when a is divided by b is",i)
