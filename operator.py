9bed1044427ac6db623f915ed3fb4d1702dfe9cb
Kanhaiya Tiwari <kanhaiyatiwari506@gmail.com>
2026-02-02 12:21:50 +0530

    simple operator ex.

---
+++ operator.py
@@ -0,0 +1,26 @@
+
+
+# Arithmetic (ints)
+a = 10  # int
+b = 3   # int
+print(a + b, type(a + b))   # 13 <class 'int'>
+print(a / b, type(a / b))   # 3.333... <class 'float'>
+
+# Comparison (bool)
+x = 5  # int
+y = 2  # int
+print(x > y, type(x > y))   # True <class 'bool'>
+
+# Logical (bool)
+p = True   # bool
+q = False  # bool
+print(p and q, type(p and q))  # False <class 'bool'>
+
+# String concatenation (str)
+s1 = "hi"  # str
+s2 = "!"   # str
+print(s1 + s2, type(s1 + s2))  # hi! <class 'str'>
+
+# Membership (bool)
+lst = [1, 2, 3]  # list
+print(2 in lst, type(2 in lst))  # True <class 'bool'>
