33ac6494b2c2405a4dc991f79b3b663e381fe07b
Kanhaiya Tiwari <kanhaiyatiwari506@gmail.com>
2026-02-02 21:36:58 +0530

    new file dic add

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+dics = {
+    "key" : "value",
+    "name" : "kanhaiya",
+    "age" : 22,
+    "is adult" : True,
+    "marks" : 89.8,
+   "subjects" : ["maths", "science", "english"],
+   "topic" : {"chapter1" : "introduction", "chapter2" : "data types"}
+}
+print(dics)
+print(dics["kanhaiya"])
