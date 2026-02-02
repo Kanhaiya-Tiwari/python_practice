a829f96c287e195c3111cdf59877bd74880db5c1
Kanhaiya Tiwari <kanhaiyatiwari506@gmail.com>
2026-02-02 21:13:09 +0530

    add new file list.py

---
+++ list.py
@@ -0,0 +1,10 @@
+marks = [92.7, 87.3, 32.3, 45.3, 383]
+bucket = ["karan", 86, "delhi",35.3]
+print(marks)
+print(type(marks))
+print (marks[1])
+print(bucket)
+print(type(bucket))
+print(bucket[2])
+print(marks[1:4])
+print(marks.append(78))
