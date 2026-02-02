97c9889665d49ff4c9b98d178bfb9c403a65cb0d
Kanhaiya Tiwari <kanhaiyatiwari506@gmail.com>
2026-02-02 09:34:33 +0530

    push first.py

---
+++ first.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#!/usr/bin/env python3
+name = "kanha"
+age = 22
+height = 284.3
+print (name)
+print (age)
+print (height)
+print (type(name))
+print (type(age))
+print (type(height))
